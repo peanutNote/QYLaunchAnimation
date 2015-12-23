c88a72927874be89bdffd13561595937d38ee785
qianye <422794901@qq.com>
2015-12-23 16:47:44 +0800

    Initial Commit

---
+++ QYLanuchAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYLanuchAnimation
+//
+//  Created by qianye on 15/12/23.
+//  Copyright © 2015年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
